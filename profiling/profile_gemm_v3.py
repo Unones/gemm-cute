38b764685368312f5cafc594ebdad0b9c67ed7fc
Unones <229073236+Unones@users.noreply.github.com>
2026-08-26 22:48:45 +0200

    New gemm_v3 with partial swizzling and corrected vectorization

---
+++ profiling/profile_gemm_v3.py
@@ -0,0 +1,25 @@
+import torch
+from gemm_v3 import gemm_v3
+
+def profile_gemm_v3():
+    M = 2048
+    N = 2048
+    K = 2048
+    
+    dtype = torch.bfloat16
+    device = torch.device("cuda:0")
+    
+    a = torch.randn((M, K), dtype=dtype, device=device)
+    b = torch.randn((K, N), dtype=dtype, device=device)
+    c = torch.randn((M, N), dtype=dtype, device=device)
+    
+    d = gemm_v3(a, b, c)
+    
+    torch.cuda.synchronize()
+    
+    torch.cuda.cudart().cudaProfilerStart()
+    d = gemm_v3(a, b, c)
+    torch.cuda.cudart().cudaProfilerStart()
+    
+if __name__ == "__main__":
+    profile_gemm_v3()
---
+++ tests/test_gemm_v2.py
@@ -0,0 +1,22 @@
+import torch
+import pytest
+
+from gemm_v2 import gemm_v2
+
+@pytest.mark.parametrize("M", [256, 512, 1024, 2048, 4096])
+@pytest.mark.parametrize("N", [256, 512, 1024, 2048, 4096])
+@pytest.mark.parametrize("K", [256, 512, 1024, 2048, 4096])
+def test_gemm_v2(M, N, K):
+    
+    device = torch.device("cuda:0")
+    dtype = torch.bfloat16
+    
+    a = torch.randn((M, K), dtype=dtype, device=device)
+    b = torch.randn((K, N), dtype=dtype, device=device)
+    c = torch.randn((M, N), dtype=dtype, device=device)
+    
+    d = gemm_v2(a, b, c)
+    
+    d_test = (a.float()@b.float() +c.float()).to(dtype=dtype)
+    
+    torch.testing.assert_close(d, d_test, atol=1e-2, rtol=1e-2)
