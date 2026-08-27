2249aed3b5a966339635599cd0056f62a01011df
Unones <229073236+Unones@users.noreply.github.com>
2026-08-27 23:32:07 +0200

    Add host function for gemm_v3

---
+++ src/gemm_v3.py
@@ -0,0 +1,237 @@
+import torch
+import cutlass
+import cutlass.cute as cute
+from math import prod
+
+from cutlass.cute.runtime import from_dlpack
+
+
+@cute.kernel
+def _kernel_gemm_v3(
+    mA : cute.Tensor,
+    mB : cute.Tensor,
+    mC : cute.Tensor,
+    mD : cute.Tensor,
+    tiled_mma : cute.TiledMma,
+    tiled_copy_async_mk : cute.TiledCopy,
+    tiled_copy_async_nk : cute.TiledCopy,
+    copy_atom_s2r : cute.CopyAtom,
+    copy_atom_g2r : cute.CopyAtom,
+    nb_tiled_k : cutlass.Constexpr,
+    bs_m : cutlass.Constexpr,
+    bs_n : cutlass.Constexpr,
+    bs_k : cutlass.Constexpr,
+    
+):
+    """
+    
+    
+    """
+    
+
+
+@cute.jit
+def _host_kernel_gemm_v3(
+    mA : cute.Tensor,
+    mB : cute.Tensor,
+    mC : cute.Tensor,
+    mD : cute.Tensor,
+):
+    """
+    
+    """
+    
+    ## Instanciating the tiled_mma
+    shape_mnk = (16, 8, 16)
+    atom_layout_mnk = (2, 2, 1)
+    
+    op_mma = cute.nvgpu.warp.MmaF16BF16Op(
+        cutlass.BFloat16,
+        cutlass.Float32,
+        shape_mnk,
+    )
+    
+    permutation_mnk = (
+        shape_mnk[0] * atom_layout_mnk[0] * 2,
+        shape_mnk[1] * atom_layout_mnk[1] * 4,
+        shape_mnk[2] * atom_layout_mnk[2],
+    )
+    
+    tiled_mma = cute.make_tiled_mma(
+        op_mma,
+        atom_layout_mnk,
+        permutation_mnk,
+    )
+    ##
+    
+    ## Constraints imposed by tiled mma
+    nb_warps = prod(atom_layout_mnk)
+    nb_threads = 32 * nb_warps      # 32 threads in each warp
+    
+    bs_m = cute.size(permutation_mnk[0])
+    bs_n = cute.size(permutation_mnk[1])
+    bs_k = cute.size(permutation_mnk[2])
+    ##
+    
+    ## Set up for tiled_copy
+    nb_elems_tile_mk = bs_m * bs_k
+    nb_elems_tile_nk = bs_n * bs_k
+    nb_elems_tile_mn = bs_m * bs_n
+    
+    nb_elems_per_thr_tile_mk = nb_elems_tile_mk // nb_threads
+    nb_elems_per_thr_tile_nk = nb_elems_tile_nk // nb_threads
+    nb_elems_per_thr_tile_mn = nb_elems_tile_mn // nb_threads
+    
+    val_layout_mk = cute.make_ordered_layout((1, nb_elems_per_thr_tile_mk), order=(1, 0))
+    val_layout_nk = cute.make_ordered_layout((1, nb_elems_per_thr_tile_nk), order=(1, 0))
+    val_layout_mn = cute.make_ordered_layout((1, nb_elems_per_thr_tile_mn), order=(1, 0))
+    
+    thr_layout_mk = cute.make_ordered_layout((bs_m, cute.ceil_div(bs_k, nb_elems_per_thr_tile_mk)), order=(1, 0))
+    thr_layout_nk = cute.make_ordered_layout((bs_n ,cute.ceil_div(bs_k, nb_elems_per_thr_tile_nk)), order=(1, 0))
+    thr_layout_mn = cute.make_ordered_layout((bs_m, cute.ceil_div(bs_n, nb_elems_per_thr_tile_mn)), order=(1, 0))
+    
+    tiler_mk, layout_tv_mk = cute.make_layout_tv(
+        thr_layout_mk,
+        val_layout_mk
+    )
+    tiler_nk, layout_tv_nk = cute.make_layout_tv(
+        thr_layout_nk,
+        val_layout_nk,
+    )
+    tiler_mn, _ = cute.make_layout_tv(
+        thr_layout_mn,
+        val_layout_mn,
+    )
+    ##
+    
+    print(f"tiler_mn : {tiler_mn} || tiler_mk : {tiler_mk} || tiler_nk : {tiler_nk}")
+    
+    ## Create copy atoms and tiled copy
+    op_atom_async = cute.nvgpu.cpasync.CopyG2SOp()
+    op_atom_s2r = cute.nvgpu.CopyS2ROp()
+    op_atom_g2r = cute.nvgpu.CopyG2ROp()
+    
+    copy_atom_async_mk = cute.make_copy_atom(
+        op_atom_async,
+        mA.dtype,
+        num_bits_per_copy=128,
+    )
+    copy_atom_async_nk = cute.make_copy_atom(
+        op_atom_async,
+        mB.dtype,
+        num_bits_per_copy=16,
+    )
+
+    tiled_copy_async_mk = cute.make_tiled_copy(
+        copy_atom_async_mk,
+        layout_tv_mk,
+        tiler_mk,
+    )
+    tiled_copy_async_nk = cute.make_tiled_copy(
+        copy_atom_async_nk,
+        layout_tv_nk,
+        tiler_nk,
+    )
+    
+    copy_atom_s2r = cute.make_copy_atom(
+        op_atom_s2r,
+        mA.dtype,
+        num_bits_per_copy=128,
+    )
+    copy_atom_g2r = cute.make_copy_atom(
+        op_atom_g2r,
+        mC.dtype,
+        num_bits_per_copy=128,
+    )
+    ##
+    
+    ## Paritioning tensors for kernel
+    M = cute.size(mC, mode=[0])
+    N = cute.size(mC, mode=[1])
+    K = cute.size(mB, mode=[1])
+    
+    mA = cute.zipped_divide(mA, tiler_mk)
+    mB = cute.zipped_divide(mB, tiler_nk)
+    mC = cute.zipped_divide(mC, tiler_mn)
+    mD = cute.zipped_divide(mD, tiler_mn)
+    ##
+    
+    ## Calculating grid
+    grid_m = cute.ceil_div(M, bs_m)
+    grid_n = cute.ceil_div(N, bs_n)
+    nb_tiles_k = cute.ceil_div(K, bs_k)
+    
+    args = (
+        mA, mB, mC, mD,
+        tiled_mma,
+        tiled_copy_async_mk,
+        tiled_copy_async_nk,
+        copy_atom_s2r,
+        copy_atom_g2r,
+        nb_tiles_k,
+        bs_m, bs_n, bs_k,
+    )
+    
+    _kernel_gemm_v3(*args).launch(
+        grid=[grid_m, grid_n, 1],
+        block=[nb_threads, 1, 1],
+    )
+    
+    
+    
+    
+
+def gemm_v3(
+    a : torch.Tensor,
+    b : torch.Tensor,
+    c : torch.Tensor,
+):
+    """
+    Perform a GEMM operation : D = A@B +C.
+    
+    Parameters
+    ----------
+    a
+        The first input tensor, shape (M, K).
+    b
+        The second input tensor, shape (K, N).
+    c
+        The third input tensor, shape (M, N).
+    
+    Returns
+    -------
+    d
+        The output tensor, shape (M, N).
+    
+    """
+    
+    ## Adapting tensor to MMA shapes
+    b = b.permute(1, 0)
+    
+    d = torch.empty_like(c)
+    ##
+    
+    ## Adapting tensors to CuTe tensor
+    a_ = from_dlpack(a, assumed_align=16)
+    b_ = from_dlpack(b, assumed_align=16)
+    c_ = from_dlpack(c, assumed_align=16)
+    d_ = from_dlpack(d, assumed_align=16)
+    ##
+    
+    _host_kernel_gemm_v3(a_, b_, c_, d_)
+    
+    
+
+if __name__ == "__main__":
+    M = 16
+    N = 16
+    K = 16
+    
+    dtype = torch.bfloat16
+    device = torch.device('cuda:0')
+    
+    a = torch.randn((M, K), dtype=dtype, device=device)
+    b = torch.randn((K, N), dtype=dtype, device=device)
+    c = torch.randn((M, N), dtype=dtype, device=device)
+    
+    gemm_v3(a, b, c)
